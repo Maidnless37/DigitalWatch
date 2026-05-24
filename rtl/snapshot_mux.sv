66c764a4da1206c36c8d548725bdc4349fb3ce12
Maidnless37 <etzolakis7@gmail.com>
2026-05-24 02:07:58 +0000

    snapshot_mux

---
+++ rtl/snapshot_mux.sv
@@ -0,0 +1,30 @@
+// Snapshot Multiplexer
+
+// When hold is low, q follows d immediately
+// When hold is high, q shows the last snapshot of d
+// A snnapshot of d is captured on each rising edge of the clock while hold is low
+
+`timescale 1ns / 1ps
+
+module snapshot_mux #(
+    parameter int WIDTH = 1
+) (
+    input logic clk,
+    input logic hold,
+    input logic [WIDTH - 1:0] d,
+    output logic [WIDTH - 1:0] q
+);
+
+  // Stores the most recent sampled value of d
+  logic [WIDTH - 1:0] snapshot = '0;
+
+  // Capture d on each rising clock edge whilse hold is 0
+  always_ff @(posedge clk) begin
+    if (!hold) begin  // Begin if hold is 0
+      snapshot <= d;  // Take snapshot of input D
+    end
+  end
+
+  assign q = hold ? snapshot : d;  // If hold is 1, output = snapshot, else output the input
+
+endmodule
